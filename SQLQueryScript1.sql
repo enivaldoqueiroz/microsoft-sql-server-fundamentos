f19d9e27909851f34faa66b68c645d2b5d6a4caa
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-28 13:11:21 -0300

    Primeiro commit - Descrição e criação do banco dbLojaJailson

---
+++ SQLQueryScript1.sql
@@ -0,0 +1,28 @@
+/**
+	* Loja de sucos do Jailson
+
+	** Vende sucos (principalmente de laranja)
+
+	** Dados dos clientes:
+		* nome (texto)
+		* data nascimento (dd/mm/aaaa)
+		* sexo (numero)
+		* endereco 
+			rua (texto)
+			cidade (texto)
+			estado (texto)
+		* teledones (texto)
+	
+	** Dados das vendas
+		* numero da venda (numero)
+		* cliente que comprou (texto)
+		* preco total (numero)
+	
+	** Dados do produto
+		* nome (texto)
+		* preco (numero)
+
+*/
+
+-- Nova base de dados 
+CREATE DATABASE dbLojaJailson;

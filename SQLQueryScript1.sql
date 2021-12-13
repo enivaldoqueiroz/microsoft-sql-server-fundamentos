953aa4ed56cd94dd901e7315bc83c219adcc612d
enivaldo queiroz <jose.costa@fitbank.com.br>
2021-12-13 14:25:17 -0300

    Inserindo dados na tabela

--- SQLQueryScript1.sql
+++ SQLQueryScript1.sql
@@ -36,14 +36,16 @@
 CREATE DATABASE dbLojaJailson;
 
 -- Criacao da tabela produtos
-CREATE TABLE tb_DadosProduto
-(
+CREATE TABLE tb_Produtos
+(	
+	cdProduto INT PRIMARY KEY IDENTITY(1,1),
     nmProduto VARCHAR(50) NOT NULL, -- Nome Produto é obrigatorio
     vlProduto DECIMAL(6,2) NOT NULL
 );
 
 CREATE TABLE tb_Clientes
 (
+	cdCliente INT PRIMARY KEY IDENTITY(1,1),
     nmCliente VARCHAR(50) NOT NULL,
     dtNascimento DATE,
     inSexo VARCHAR(1) NOT NULL,
@@ -53,3 +55,19 @@ CREATE TABLE tb_Clientes
     nmTelefone1 VARCHAR(15),
     nmTelefone2 VARCHAR(15)
 );
+
+CREATE TABLE tb_Vendas
+(
+	cdVenda INT PRIMARY KEY IDENTITY(1,1),
+	cdProduto INT NOT NULL,
+	vlProduto DECIMAL(6,2) NOT NULL
+);
+
+-- Regra - Constraint
+ALTER TABLE tb_Vendas
+ADD CONSTRAINT FK_Produto_Vendas
+FOREIGN KEY (cdProduto)
+REFERENCES tb_Produtos (cdProduto);
+
+-- Inserção nas tabelas
+INSERT INTO tb_Produtos VALUES ('Suco de laranja', 4,5);

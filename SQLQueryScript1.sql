77721cc35fa5c37049855adb09bbc9541fc55e48
enivaldo queiroz <jose.costa@fitbank.com.br>
2021-12-13 13:59:47 -0300

    Implementando as chaves primarias na tabelas

--- SQLQueryScript1.sql
+++ SQLQueryScript1.sql
@@ -24,11 +24,32 @@
 
 */
 
+/*
+ - NOTAS:
+	- Truncar: Diminuir o tamanho do texto/dado
+	Exemplo: nmProduto VARCHAR(3);
+	"Coxinha" tem 7 letras, então a saida ficarar -> 'Cox'
+
+*/
+
 -- Nova base de dados 
 CREATE DATABASE dbLojaJailson;
 
 -- Criacao da tabela produtos
-CREATE TABLE tb_DadosProdutos(
+CREATE TABLE tb_DadosProduto
+(
     nmProduto VARCHAR(50) NOT NULL, -- Nome Produto é obrigatorio
     vlProduto DECIMAL(6,2) NOT NULL
+);
+
+CREATE TABLE tb_Clientes
+(
+    nmCliente VARCHAR(50) NOT NULL,
+    dtNascimento DATE,
+    inSexo VARCHAR(1) NOT NULL,
+    nmEndereco VARCHAR(50),
+    nmCidade VARCHAR(50),
+    nmEstado VARCHAR(30),
+    nmTelefone1 VARCHAR(15),
+    nmTelefone2 VARCHAR(15)
 );
